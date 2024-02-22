cfeea5ed1428db05fc27d224cc9c6c8aa617e6b6
Joelutri <joel.utriainen@edu.salpaus.fi>
2024-02-22 17:23:11 +0000

    half way done

--- src/games.js
+++ src/games.js
@@ -1,10 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Retrieve player details from the URL query parameters
-    const urlParams = new URLSearchParams(window.location.search);
-    const playerName = urlParams.get('name');
-    const playerRank = urlParams.get('rank');
+    const urlPar = new URLSearchParams(window.location.search);
+    const playerName = urlPar.get('name');
+    const playerRank = urlPar.get('rank');
 
-    // Update the HTML with the retrieved details
     document.getElementById('player-name').textContent = `Player Name: ${playerName}`;
     document.getElementById('rank').textContent = `Rank: ${playerRank}`;
     document.getElementById('games').textContent = `Rank: ${''}`;
