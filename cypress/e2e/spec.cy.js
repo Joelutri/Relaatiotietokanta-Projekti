b118a1577ea47a4fb9c872c90738684351968d6b
Joelutri <joel.utriainen@edu.salpaus.fi>
2024-03-15 07:47:06 +0000

    GG

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,10 +2,11 @@ describe('template spec', () => {
     it ('passes', () => {
         cy.visit('https://projectdatabase.azurewebsites.net/src/pages/index.html')
         cy.get('a').contains('Dev tools').click()
+        /*
         cy.window().then(win => {
-            cy.stub(win, 'prompt').returns('Cypress');
+            cy.stub(win, 'prompt').returns('cypressTest');
           });
-       /* cy.get('.postPrompt').contains('New Rank').click()
+        cy.get('.postPrompt').contains('New Rank').click()
         cy.on('window:confirm', () => true);
 
         cy.get('.postPrompt').contains('New Champion').click()
@@ -15,19 +16,19 @@ describe('template spec', () => {
         cy.on('window:confirm', () => true);
         */
         cy.get('a').contains('New Game').click()
+        /*
         cy.get('.linkTXT').contains('Create player').click()
         cy.get('#playerName').type('cypressTest')
         cy.get('#rankNameInput').type('cypressTest')
-        cy.get('#newPlayer').contains('Send').click()
+        cy.get('.postSubmit').contains('Send').click()
         cy.get('button').contains('Back').click()
-
+*/
         cy.get('#championNameInput').type('cypressTest')
         cy.get('#playerNameInput').type('cypressTest')
         cy.get('#skinNameInput').type('cypressTest')
-        cy.get('#newGame').contains('Send').click()
+        cy.get('.postSubmit').contains('send').click()
 
         cy.get('a').contains('Players').click()
         cy.get('#data-list div').contains('cypressTest').click()
-        cy.get('button').contains('back').click()
     })
 })
